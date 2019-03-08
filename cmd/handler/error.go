5928059da591228c1594f0c49fddac72537276a3
am91060@ens.etsmtl.ca <am91060@ens.etsmtl.ca>
2019-03-08 17:42:50 -0500

    Added error handling for 401 Unauthorized.

--- cmd/handler/error.go
+++ cmd/handler/error.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"fmt"
 	"net/http"
+
+	"azure.com/ecovo/trip-search-service/cmd/middleware/auth"
 )
 
 // An Error is an application error that can be handled by a handler.
@@ -21,11 +23,13 @@ func (err Error) String() string {
 func WrapError(err error) *Error {
 	if err == nil {
 		return nil
-	}
-
-	return &Error{
-		http.StatusInternalServerError,
-		"Something went wrong while processing your request. Please contact your system administrator.",
-		err,
+	} else if _, ok := err.(auth.UnauthorizedError); ok {
+		return &Error{http.StatusUnauthorized, "unauthorized", err}
+	} else {
+		return &Error{
+			http.StatusInternalServerError,
+			"Something went wrong while processing your request. Please contact your system administrator.",
+			err,
+		}
 	}
 }
